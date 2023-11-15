5098fca5c30238d3645f68b3be10e1ba7ce9f120
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-11-15 16:52:07 +0200

    add yargs

--- index.js
+++ index.js
@@ -5,30 +5,30 @@ const {
   removeContact,
 } = require("./contacts");
 
-// const argv = require("yargs").argv;
+const argv = require("yargs").argv;
 
 async function invokeAction({ action, id, name, email, phone }) {
   try {
     switch (action) {
       case "list":
         const contacts = await listContacts();
-        console.log("List of contacts:", contacts);
+        console.table(contacts);
         break;
 
       case "get":
         const contactById = await getContactById(id);
         if (!contactById) throw new Error(`Contact with id: ${id} not found!`);
-        console.log("Contact by ID:", contactById);
+        console.table(contactById);
         break;
 
       case "add":
         const newContact = await addContact(name, email, phone);
-        console.log("Added contact:", newContact);
+        console.table(newContact);
         break;
 
       case "remove":
         const removedContact = await removeContact(id);
-        console.log("Removed contact:", removedContact);
+        console.table(removedContact);
         break;
 
       default:
@@ -39,14 +39,4 @@ async function invokeAction({ action, id, name, email, phone }) {
   }
 }
 
-// invokeAction(argv);
-
-// invokeAction({ action: "list"});
-// invokeAction({ action: "get", id: "1DEXoP8AuCGYc1YgoQ6hw" });
-// invokeAction({
-//   action: "add",
-//   name: "Hryhorii Kyriienko",
-//   email: "hrhrkrnk@gmail.com",
-//   phone: +380966100113,
-// });
-// invokeAction({ action: "remove", id: "BM4NNUD7nMMDwhUYo-bRN" });
+invokeAction(argv);
