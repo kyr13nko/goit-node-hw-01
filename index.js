5b76cb8afa6645a5a882ad2a20483f8179e3c238
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-11-15 23:55:46 +0200

    remove trycatch

--- index.js
+++ index.js
@@ -21,34 +21,30 @@ program.parse(process.argv);
 const argv = program.opts();
 
 async function invokeAction({ action, id, name, email, phone }) {
-  try {
-    switch (action) {
-      case "list":
-        const contacts = await listContacts();
-        console.table(contacts);
-        break;
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
 
-      case "get":
-        const contactById = await getContactById(id);
-        if (!contactById) throw new Error(`Contact with id: ${id} not found!`);
-        console.table(contactById);
-        break;
+    case "get":
+      const contactById = await getContactById(id);
+      if (!contactById) throw new Error(`Contact with id: ${id} not found!`);
+      console.table(contactById);
+      break;
 
-      case "add":
-        const newContact = await addContact(name, email, phone);
-        console.table(newContact);
-        break;
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.table(newContact);
+      break;
 
-      case "remove":
-        const removedContact = await removeContact(id);
-        console.table(removedContact);
-        break;
+    case "remove":
+      const removedContact = await removeContact(id);
+      console.table(removedContact);
+      break;
 
-      default:
-        console.warn("\x1B[31m Unknown action type!");
-    }
-  } catch (error) {
-    console.error("Error:", error.message);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
   }
 }
 
